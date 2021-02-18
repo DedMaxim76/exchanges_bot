25bd95bb531c4e37a1fe58cb3094805c853dda91
DedMaxim76 <tobilko01@gmail.com>
2021-02-18 20:18:44 +0200

    finnal

--- data/config.py
+++ data/config.py
@@ -1,4 +1,5 @@
 from environs import Env
+
 # Теперь используем вместо библиотеки python-dotenv библиотеку environs
 
 env = Env()
@@ -8,12 +9,10 @@ BOT_TOKEN = env.str("BOT_TOKEN")  # Забираем значение типа s
 ADMINS = env.list("ADMINS")  # Тут у нас будет список из админов
 IP = env.str("ip")  # Тоже str, но для айпи адреса хоста
 
-
-
-db_host = IP
+DB_HOST = env.str("DB_HOST")
 
 PGUSER = env.str("PGUSER")
 PGPASSWORD = env.str("PGPASSWORD")
 DATABASE = env.str("DATABASE")
 
-POSTGRES_URI = f"postgresql://{PGUSER}:{PGPASSWORD}@{db_host}/{DATABASE}"
+POSTGRES_URI = f"postgresql://{PGUSER}:{PGPASSWORD}@{DB_HOST}/{DATABASE}"
